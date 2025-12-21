74304e7a6916c94e758d83f9221c800b51fac5e1
deniskorovko <dkcreator@gmail.com>
2025-12-21 22:58:27 +0300

    FilmControllerTest fixed

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -13,18 +14,21 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FilmControllerTest {
 
+    @LocalServerPort
+    private int port;
+
     @Autowired
-    private TestRestTemplate restTemplate;
+    private TestRestTemplate testRestTemplate = new TestRestTemplate();
 
     @Test
     public void shouldReturnNotValidException(){
         Film notValidfilm = new Film(0,
                 "Some film",
                 "Very intresting film",
-                "1895-12-29",
+                "1895-12-24",
                 120);
 
-        ResponseEntity<Film> notValidResponseEntity = restTemplate.postForEntity("/api/films", notValidfilm, Film.class);
+        ResponseEntity<Film> notValidResponseEntity = testRestTemplate.postForEntity("http://localhost:" + port + "/films", notValidfilm, Film.class);
         assertEquals(HttpStatus.BAD_REQUEST, notValidResponseEntity.getStatusCode());
     }
 }
