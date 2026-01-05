371d77c566951bd8e13b62e2fa88d67911900e03
deniskorovko <dkcreator@gmail.com>
2026-01-05 10:10:43 +0300

    Fix validation with lombok annotations and constructor annotations

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
--- src/main/java/ru/yandex/practicum/filmorate/validation/groups/CreateGroup.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/groups/CreateGroup.java
@@ -2,5 +2,6 @@ package ru.yandex.practicum.filmorate.validation.groups;
 
 public interface CreateGroup{
 
-    interface onCreate{}
+    interface onCreate{
+    }
 }
